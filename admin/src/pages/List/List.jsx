3f88a4732d2ef4307e1f073fc134624ea841ecdb
TiM1113 <yuantian1113@gmail.com>
2025-01-07 19:49:44 +1030

    fix: Update API endpoints and add error handling

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,33 +1,42 @@
 import React, { useEffect, useState } from 'react'
 import './List.css'
 import axios from 'axios'
-import {toast} from "react-toastify"
+import { toast } from "react-toastify"
+import PropTypes from 'prop-types'
 
-const List = ({url}) => {
+const List = ({ url }) => {
   const [list, setList] = useState([]);
 
   const fetchList = async () => {
-    const response = await axios.get(`${url}/food/list`);
-    if (response.data.success) {
-      setList(response.data.data);
-    }
-    else{
-      toast.error("Error")
-    }
-  }
-
-  const removeFood = async(foodId) => {
-    const response = await axios.post(`${url}/food/remove`, {id:foodId})
-    await fetchList();
-    if ((response.data.success)) {
-      toast.success(response.data.message) 
-    }
-    else {
-      toast.error("Error deleting item");
+    try {
+      const response = await axios.get(`${url}/api/food/list`);
+      if (response.data.success) {
+        setList(response.data.data);
+      } else {
+        toast.error("Error loading food items");
+      }
+    } catch (error) {
+      console.error('Error fetching list:', error);
+      toast.error("Error connecting to server");
     }
   }
 
-  useEffect(()=>{
+  const removeFood = async (foodId) => {
+    try {
+      const response = await axios.post(`${url}/api/food/remove`, { id: foodId })
+      if (response.data.success) {
+        toast.success(response.data.message);
+        await fetchList();
+      } else {
+        toast.error("Error deleting item");
+      }
+    } catch (error) {
+      console.error('Error removing food:', error);
+      toast.error("Error connecting to server");
+    }
+  }
+
+  useEffect(() => {
     fetchList();
   }, [])
 
@@ -42,20 +51,24 @@ const List = ({url}) => {
           <b>Price</b>
           <b>Action</b>
         </div>
-        {list.map((item, index)=>{
-          return(
-            <div key={index} className="list-table-format">
+        {list.map((item) => {
+          return (
+            <div key={item._id} className="list-table-format">
               <img src={item.image} alt={item.name} />
               <p>{item.name}</p>
               <p>{item.category}</p>
               <p>${item.price}</p>
-              <p onClick={()=>removeFood(item._id)} className='cursor'>X</p>
+              <p onClick={() => removeFood(item._id)} className='cursor'>X</p>
             </div>
           )
         })}
       </div>
     </div>
   )
+}
+
+List.propTypes = {
+  url: PropTypes.string.isRequired,
 }
 
 export default List
